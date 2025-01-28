4dec2db4201009964a10c6ecc025245c3ac1fae6
fallinov <steve.fallet@divtec.ch>
2025-01-28 19:41:49 +0100

    correction ex3

--- src/pages/Exercice3.vue
+++ src/pages/Exercice3.vue
@@ -4,20 +4,30 @@
     <div class="exe-objectifs">
       <h2>Objectifs</h2>
       <ul>
-        <li>Créer une liste dynamique affichée sur une carte à partir d’un
-          tableau de données.
+        <li>Créer une liste dynamique de Pokémons basée sur la variable
+          <code>pokemons</code> qui contient un <strong>tableau
+            de noms de Pokémons</strong>.
         </li>
-        <li>Utiliser la directive <code>v-for</code> pour parcourir et afficher
-          les éléments de la liste.
+        <li>Utiliser la directive <code>v-for</code> pour parcourir le tableau
+          et créer les éléments de la liste <code>&lt;v-list-item&gt;</code>.
         </li>
-        <li>Ajouter un bouton permettant de supprimer un élément spécifique de
-          la liste.
+        <li>
+          Utiliser l'index, position dans le tableau,
+          comme clé <code>:key ="index"</code>
+          de la directive <code>v-for</code> pour identifier chaque élément
+          de la liste.
         </li>
-        <li>Afficher un message <strong>"La liste est vide"</strong> si le
-          tableau ne contient plus d’éléments.
+        <li>
+          Remplacer le texte <code>*** POKEMON ***</code> par le nom du Pokémon
+          parcourus dans la boucle <code>v-for</code>.
         </li>
-        <li><strong>Suggestion :</strong> Testez votre travail avec différents
-          jeux de données.
+        <li>Ecouter l'événement <code>click</code> sur le bouton de suppression
+          pour appeler la fonction <code>removePokemon(index)</code> et ainsi
+          supprimer le Pokémon.
+        </li>
+        <li>Afficher le message <code>"La liste est vide"</code> uniquement
+          si le tableau ne contient plus de Pokémons. Sinon, afficher la liste
+          des Pokémons.
         </li>
       </ul>
     </div>
@@ -27,25 +37,23 @@
       <v-card
         class="mx-auto my-6 pa-2"
         max-width="300"
-        prepend-icon="mdi-pokeball"
-        title="Pokémons"
       >
-        <v-card-subtitle v-if="pokemons.length === 0" class="text-center">
+        <v-card-title>Liste de Pokémons</v-card-title>
+
+        <v-card-subtitle class="text-center">
           La liste est vide.
         </v-card-subtitle>
-        <v-list v-else>
-          <v-list-item
-            v-for="(pokemon, index) in pokemons"
-            :key="index"
-          >
+
+        <v-list>
+          <v-list-item>
             <v-list-item-title>
-              {{ pokemon }}
+              *** POKEMON ***
             </v-list-item-title>
+
             <template v-slot:append>
               <v-btn
                 icon="mdi-delete"
                 variant="text"
-                @click="removePokemon(index)"
               ></v-btn>
             </template>
           </v-list-item>
@@ -58,10 +66,21 @@
 <script setup>
 import {ref} from 'vue';
 
-const pokemons = ref(["Pikachu", "Bulbizarre", "Salamèche", "Carapuce", "Rondoudou"]);
+// Tableau des Pokémons réactif
+const pokemons = ref([
+  "Pikachu",
+  "Bulbizarre",
+  "Salamèche",
+  "Carapuce",
+  "Rondoudou"
+]);
 
-const removePokemon = (index) => {
+/**
+ * Fonction qui supprime le Pokémon à l'index donné.
+ * @param {number} index - Index, position, du Pokémon à supprimer.
+ */
+function removePokemon(index) {
   pokemons.value.splice(index, 1);
-};
+}
 </script>
 
