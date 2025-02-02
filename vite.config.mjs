f05b955563a5ece5f6f6d0871dbef09fb2b1c0b2
fallinov <steve.fallet@divtec.ch>
2025-02-02 15:18:28 +0100

    ajout chemin base dans vite.config.mjs et htaccess

--- vite.config.mjs
+++ vite.config.mjs
@@ -11,6 +11,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'vue-bases/', // Chemin de base de l'application
   plugins: [
     VueRouter(),
     Vue({
