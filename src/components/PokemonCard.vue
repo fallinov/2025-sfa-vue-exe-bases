caf32d2ae85d1c753cba05bab6e583d699ea9334
Fallet Steve <steve.fallet@divtec.ch>
2025-02-19 11:09:22 +0100

    nouvelle solution ex8

--- src/components/PokemonCard.vue
+++ src/components/PokemonCard.vue
@@ -1,21 +1,42 @@
 <template>
-  <v-list-item class="d-flex align-center">
-    <v-list-item-title>{{ pokemonName }}</v-list-item-title>
-    <v-btn icon="mdi-delete" variant="text" @click="$emit('remove')"></v-btn>
+  <v-list-item>
+    <v-list-item-title>
+      {{ pokeNom }}
+    </v-list-item-title>
+
+    <template v-slot:append>
+      <v-btn
+        icon="mdi-delete"
+        variant="text"
+        @click="deletePokemon"
+      ></v-btn>
+    </template>
   </v-list-item>
 </template>
 
 <script setup>
-defineProps({
-  pokemonName: {
+// Définition des propriétés du composant
+const props = defineProps({
+  pokeNom: {
     type: String,
     required: true,
   },
-});
+})
+
+// Définition des événements du composant
+const emit = defineEmits(['delete'])
+
+// Fonction qui émet l'événement delete
+function deletePokemon () {
+  // Si l'utilisateur confirme la suppression
+  if(confirm(`Voulez-vous vraiment supprimer ${ props.pokeNom } ?`)) {
+    // Émission de l'événement delete
+    emit('delete')
+  }
+}
+
 </script>
 
-<style scoped>
-.v-list-item {
-  border-bottom: 1px solid #e0e0e0;
-}
+<style scoped lang="sass">
+
 </style>
